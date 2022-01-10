f3073ddf8afd2a6ab6eb45bbf013db9012032803
RazAbk <RazAb98754@Gmail.com>
2022-01-10 12:45:11 +0200

    add scss base, fonts and files structure, router & basic pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Route, Routes } from 'react-router'
+import { NotesApp } from './pages/NotesApp';
+import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>app</h1>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<NotesApp />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
---
+++ frontend/src/pages/NotesApp.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NotesApp = () => {
+    return (
+        <div className="notes-app">
+            <h1>Hi this is notes app</h1>
+        </div>
+    )
+}
