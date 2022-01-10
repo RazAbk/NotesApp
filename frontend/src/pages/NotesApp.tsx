87037b1019193393e016c0c84c86685c251fe2ca
RazAbk <RazAb98754@Gmail.com>
2022-01-10 13:13:57 +0200

    basic app layout

--- frontend/src/pages/NotesApp.tsx
+++ frontend/src/pages/NotesApp.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { INote } from '../interfaces/dataInterfaces'
+import { BiLogOut } from 'react-icons/bi'
+
+const notesData: INote[] = [
+    {
+        _id: 'asdasd',
+        title: 'do that',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, tempora at! Earum pariatur blanditiis dolores nostrum illo dolorum repellendus consequatur?'
+    }
+]
+
 
 export const NotesApp = () => {
     return (
         <div className="notes-app">
-            <h1>Hi this is notes app</h1>
+            <header>
+                <BiLogOut className="logout-btn" />
+                <div className="header-main-content">
+                    <h1 className="app-logo">NotesApp</h1>
+                    <h2>Hello User</h2>
+                </div>
+                <span></span>
+            </header>
+            <main>
+
+            </main>
         </div>
     )
 }
