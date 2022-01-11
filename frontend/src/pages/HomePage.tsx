a515bc409a9578ae207d8cc25b0cc8c0b278babb
RazAbk <RazAb98754@Gmail.com>
2022-01-11 13:12:38 +0200

    add username and password validation

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -22,16 +22,34 @@ export const HomePage = () => {
             userName: ev.target[0].value,
             password: ev.target[1].value
         }
-    
+
+        if (!isLogin) {
+            // First char is a letter, rest can be numbers, letters or symbols, 4-30 length
+            const userNameTest = /^[A-Za-z][A-Za-z0-9!@#$%^&*_]{4,30}$/
+
+            // At least on of each: uppercase letter, lowercase letter, digit, symbol, length of at least 8
+            const passwordTests = [/^[A-Za-z0-9!@#$%^&*_].{5,}$/, /[A-Z]/, /[a-z]/, /[!@#$%^&*_]/]
+
+            if (!userNameTest.test(credentials.userName)) {
+                alert('Bad username, first letter should be a letter, at least 4 characters')
+                return
+            }
+
+            if (!passwordTests.every(test => test.test(credentials.password))) {
+                alert('Weak password, must have upper and lower case letters, at least 1 digit and 1 symbol, at length of at least 5 characters')
+                return
+            }
+        }
+
         let user: any
 
-        if(!isLogin){
+        if (!isLogin) {
             user = await dispatch(signup(credentials))
         } else {
             user = await dispatch(login(credentials))
         }
 
-        if (user){
+        if (user) {
             navigate('/note')
         } else {
             console.log(`Could not ${isLogin ? 'Login' : 'Signup'}!`)
